1cd854144329025bcff4917453260abc84791913
Aleksandr <asavostin1@gmail.com>
2019-09-28 21:59:53 +0500

    Улучшения

--- build/js/util.js
+++ build/js/util.js
@@ -3,26 +3,39 @@
 (function () {
 
 var render = function (data, item, func) {
-	var fragment = document.createDocumentFragment();
+  var fragment = document.createDocumentFragment();
 
-	for (var i = 0; i < data.length; i++) {
-	  fragment.appendChild(func(data[i]));
-	};
+  for (var i = 0; i < data.length; i++) {
+     fragment.appendChild(func(data[i]));
+  };
 
-	  item.appendChild(fragment);
+    item.appendChild(fragment);
 };
 
 var deleteItem = function (classList, classItem) {
-	var linkList = document.querySelector(classList);
-	var linkItem = linkList.querySelectorAll(classItem);
+  var linkList = document.querySelector(classList);
+  var linkItem = linkList.querySelectorAll(classItem);
 
-	for (var i = 0; i < linkItem.length; i++) {
-	  linkList.removeChild(linkItem[i]);
-	}
+  for (var i = 0; i < linkItem.length; i++) {
+    linkList.removeChild(linkItem[i]);
+  }
+};
+
+window.renderPhoto = function (data, item, func) {
+  var fragment = document.createDocumentFragment();
+
+  for (var i = 0; i < data.length; i++) {
+    if (data[i].name !== "(null)") {
+      fragment.appendChild(func(data[i]));
+    }
+  };
+
+    item.appendChild(fragment);
 };
 
 window.util = {
-	render: render,
-	deleteItem: deleteItem,
+  render: render,
+  renderPhoto: renderPhoto,
+  deleteItem: deleteItem,
 };
 })();
--- source/js/searchArtist.js
+++ source/js/searchArtist.js
@@ -4,21 +4,20 @@
 
   var ENTER_KEYCODE = 13;
   var ESC_KEYCODE = 27;
-  var fieldSearch = document.querySelector('input');
+  var mainPage = document.querySelector('.main');
+  var fieldSearch = mainPage.querySelector('.main__search');
   var dataArtist;
   var dataAlbum;
-  var buttonSearch = document.querySelector('.main__button-search');
-  var link = document.querySelector('.music__item-link');
-  var linkList = document.querySelector('ul');
-  var photoList = document.querySelector('.photo__list');
-  var classLink = 'li';
-  var classList = 'ul';
-  var classPhotoDelete = '.photo__list';
-  var photoTitle = document.querySelector('.photo__title');
-  var hiddenClass = 'visually-hidden';
+  var buttonSearch = mainPage.querySelector('.main__button-search');
+  var linkList = mainPage.querySelector('.music__list');
+  var photoList = mainPage.querySelector('.photo__list');
+  var photoTitle = mainPage.querySelector('.photo__title');
   var closeButton = photoList.querySelector('.photo__close-list');
-  var mainPage = document.querySelector('.main');
   var mainPageWrapper = mainPage.querySelector('.photo__wrapper');
+  var hiddenClass = 'visually-hidden';
+  var classPhotoDelete = '.photo__list';
+  var linkClass = 'li';
+  var listClass = 'ul';
 
   mainPageWrapper.classList.add(hiddenClass);
 
@@ -43,47 +42,43 @@
   var getPhoto = function (data) {
     var photoTemplate = document.querySelector('#photo').content.querySelector('.photo__item');
     var photoElement = photoTemplate.cloneNode(true);
-    var checkPhoto = data.name === '';
-    var checkName = data.image[2]['#text'] === '';
+    var checkName = data.name === '';
+    var checkPhoto = data.image[2]['#text'] === '';
 
 
     var photo = photoElement.querySelector('.photo__album');
-    if (!checkName) {
+    if (!checkPhoto) {
       photo.src = data.image[2]['#text'];
     } else {
-       photoElement.removeChild(photo);
+       photo.src = 'img/poster_none.png';
      };
 
     var photoName = photoElement.querySelector('.photo__name');
-    if (!checkPhoto) {
+    if (!checkName) {
       photoName.textContent = data.name;
     } else {
        photoElement.removeChild(photoName);
-     };
+     }
 
-     if (!checkPhoto || !checkName) {
-        return photoElement;
-     };
+      return photoElement;
   };
 
   var errorHandler = function () {
     var templateError = document.querySelector('#error').content.querySelector('.error');
-    var tagMain = document.querySelector('body');
     var fragment = document.createDocumentFragment();
 
     fragment.appendChild(templateError);
-    tagMain.appendChild(fragment);
+    mainPage.appendChild(fragment);
   };
 
   var messageNotFound = function () {
     var message = document.createElement('li');
     message.classList.add('notFound');
     message.textContent = 'По вашему запросу не найдено ни одного исполнителя';
-    var list = document.querySelector('ul');
     var fragment = document.createDocumentFragment();
 
     fragment.appendChild(message);
-    list.appendChild(fragment);
+    linkList.appendChild(fragment);
   };
 
   fieldSearch.addEventListener('input', function () {
@@ -99,18 +94,18 @@
     buttonSearch.setAttribute('disabled', 'disabled');
 
     if (document.querySelector('.photo__item')) {
-       window.util.deleteItem(classPhotoDelete, classLink);
+       window.util.deleteItem(classPhotoDelete, linkClass);
     };
 
     setTimeout(function() {
       if (dataArtist.length == 0) {
-        window.util.deleteItem(classList, classLink);
+        window.util.deleteItem(listClass, linkClass);
         messageNotFound();
         fieldSearch.value = '';
       }
 
       else if (fieldSearch.value != '') {
-        window.util.deleteItem(classList, classLink);
+        window.util.deleteItem(listClass, linkClass);
         window.util.render(dataArtist, linkList, getLink);
         fieldSearch.value = '';
       }
@@ -126,23 +121,22 @@
       buttonSearch.setAttribute('disabled', 'disabled');
 
       if (document.querySelector('.photo__item')) {
-         window.util.deleteItem(classPhotoDelete, classLink);
+         window.util.deleteItem(classPhotoDelete, linkClass);
       };
 
       setTimeout(function () {
         if (dataArtist.length == 0) {
-          window.util.deleteItem(classList, classLink);
+          window.util.deleteItem(listClass, linkClass);
           messageNotFound();
           fieldSearch.value = '';
         }
 
         else if (fieldSearch.value != '') {
-          window.util.deleteItem(classList, classLink);
+          window.util.deleteItem(listClass, linkClass);
           window.util.render(dataArtist, linkList, getLink);
           fieldSearch.value = '';
         }
-    }, 1000);
-
+      }, 1000);
     }
   });
 
@@ -154,7 +148,7 @@
       window.load(successHandlerAlbum, errorHandler, ALBUM);
 
       setTimeout(function () {
-        window.util.render(dataAlbum, photoList, getPhoto,);
+        window.util.renderPhoto(dataAlbum, photoList, getPhoto,);
         photoTitle.textContent = linkText;
         closeButton.classList.remove(hiddenClass);
         mainPageWrapper.classList.toggle(hiddenClass);
@@ -163,14 +157,16 @@
   });
 
   closeButton.addEventListener('click', function () {
-    window.util.deleteItem(classPhotoDelete, classLink);
+    window.util.deleteItem(classPhotoDelete, linkClass);
     mainPageWrapper.classList.toggle(hiddenClass);
   });
 
   document.addEventListener('keydown', function (evt) {
-    if (evt.keyCode === ESC_KEYCODE) {
-      window.util.deleteItem(classPhotoDelete, classLink);
-      mainPageWrapper.classList.toggle(hiddenClass);
+    if (document.querySelector('.photo__item')) {
+      if (evt.keyCode === ESC_KEYCODE) {
+        window.util.deleteItem(classPhotoDelete, linkClass);
+        mainPageWrapper.classList.toggle(hiddenClass);
+      }
     }
   });
 })()
